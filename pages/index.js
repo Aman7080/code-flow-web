7663f4055c185fcdab28d0feca080dab56aafb4c
Aman Kumar <kumar7080aman7080@gmail.com>
2022-10-21 05:19:54 +0530

    extra style removed

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,47 @@ export default function Post(props) {
               cardType: "summary_large_image",
             }}
           />
-          <Container>
+
+          {/* Adding elements here _______________________________*/}
+          <section className=" flex-col justify-center items-start p-0 min-h-screen mx-auto my-0 max-w-4xl">
+            <div className="transition delay-100 block">
+              <h1 className=" text-cyan-300 font-medium font-mono mb-7 ml-1 leading-3">Hi, my name is</h1>
+            </div>
+            <div className="transition delay-200 block">
+              <h2 className=" text-4xl text-slate-300 font-bold m-0">Aman Kumar</h2>
+            </div>
+            <div className="transition delay-300 block" >
+              <h3 className="text-4xl text-slate-400 font-semibold mt-3">I have started building things for the web.</h3>
+            </div>
+            <div className="transition delay-400" >
+              <p className=" max-w-lg font-sans leading-4 font-light mt-8">
+                I’m a software engineer specializing in building (and
+                occasionally designing) exceptional digital experiences.
+                Currently, I’m focused on building accessible, human-centered
+                products at{" "}
+                <a
+                  href="https://upstatement.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Upstatement
+                </a>
+                .
+              </p>
+            </div>
+            <div className="transition delay-500" >
+              <a
+                className="email-link"
+                href="https://www.newline.co/courses/build-a-spotify-connected-app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Check out my course!
+              </a>
+            </div>
+          </section>
+
+          {/* <Container>
             <div className="grid gap-10 lg:gap-10 md:grid-cols-2 ">
               {postdata.slice(0, 2).map((post) => (
                 <PostList
@@ -62,7 +102,7 @@ export default function Post(props) {
                 <PostList key={post._id} post={post} aspect="square" />
               ))}
             </div>
-          </Container>
+          </Container> */}
         </Layout>
       )}
     </>
@@ -70,7 +110,7 @@ export default function Post(props) {
 }
 
 export async function getStaticProps({ params, preview = false }) {
-    const post = await getClient(preview).fetch(homePostquery());
+  const post = await getClient(preview).fetch(homePostquery());
   const config = await getClient(preview).fetch(configQuery);
   return {
     props: {
